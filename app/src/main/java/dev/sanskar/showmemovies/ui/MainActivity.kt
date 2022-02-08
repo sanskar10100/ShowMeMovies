7d37a9493b366654946b6318080da1b79bb80e06
Sanskar Agrawal <sanskar10100@gmail.com>
2022-02-08 11:00:57 +0530

    Base Project Init

---
+++ app/src/main/java/dev/sanskar/showmemovies/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.sanskar.showmemovies.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.sanskar.showmemovies.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/dev/sanskar/showmemovies/ui/home/HomeFragment.kt
@@ -0,0 +1,12 @@
+package dev.sanskar.showmemovies.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.sanskar.showmemovies.R
+
+class HomeFragment : Fragment() {
+
+}
