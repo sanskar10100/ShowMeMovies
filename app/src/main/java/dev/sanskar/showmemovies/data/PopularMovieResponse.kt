578f22ebe479f11d255c1bdfc74c175bb6bbfdf4
Sanskar Agrawal <sanskar10100@gmail.com>
2022-02-08 11:05:55 +0530

    Add data model

---
+++ app/src/main/java/dev/sanskar/showmemovies/data/PopularMovieResponse.kt
@@ -0,0 +1,8 @@
+package dev.sanskar.showmemovies.data
+
+data class PopularMovieResponse(
+    val page: Int,
+    val results: List<Result>,
+    val total_pages: Int,
+    val total_results: Int
+)
